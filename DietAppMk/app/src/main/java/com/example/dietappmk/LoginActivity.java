a6940403872360adb202529098a47d352ec37450
Manish Kumar Vats <mvats77947794@gmail.com>
2019-10-19 20:18:17 +0530

    init commit

---
+++ DietAppMk/app/src/main/java/com/example/dietappmk/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.dietappmk;
+
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView emailTextView=findViewById(R.id.email);
+        TextView passwordTextView=findViewById(R.id.password);
+
+    }
+
+    public void signInBtn(View view) {
+
+        Intent i = new Intent(LoginActivity.this, FullscreenActivity.class);
+        startActivity(i);
+
+    }
+}
